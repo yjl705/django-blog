39720ab675bc3c6120a157ae1f8b456c4db8a9ea
Jialuo Yuan <yjl705@163.com>
2020-11-20 16:15:38 +0800

    detail and WebTitle

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from .models import Post, Category, Tag
 import re
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class IndexView(ListView):
     model = Post
@@ -21,7 +21,7 @@ def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list':post_list})
 
-def detail(request, pk):
+'''def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     # 阅读量 +1
@@ -39,18 +39,54 @@ def detail(request, pk):
     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
     post.toc = m.group(1) if m is not None else ''
 
-    return render(request, 'blog/detail.html', context={'post': post})
+    return render(request, 'blog/detail.html', context={'post': post})'''
+# 记得在顶部导入 DetailView
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super().get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1) if m is not None else ''
+
+        return post
 
 class ArchiveView(IndexView):
     def get_queryset(self):
 
         return super(ArchiveView, self).get_queryset().filter(created_time__year=year,
                                     created_time__month=month).order_by('-created_time')
-def archive(request, year, month):
+'''def archive(request, year, month):
     post_list = Post.objects.filter(created_time__year=year,
                                     created_time__month=month
                                     ).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list':post_list})
+    return render(request, 'blog/index.html', context={'post_list':post_list})'''
 
 
 class CategoryView(ListView):
@@ -62,10 +98,10 @@ class CategoryView(ListView):
         cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
         return super(CategoryView, self).get_queryset().filter(category=cate)
 
-def category(request, pk):
+'''def category(request, pk):
     cate = get_object_or_404(Category, pk = pk)
     post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list':post_list})
+    return render(request, 'blog/index.html', context={'post_list':post_list})'''
 
 def author(request, pk):
     #post = get_object_or_404(Post, pk = pk) #不知道为什么，这边Post为空
@@ -78,10 +114,10 @@ class TagView(IndexView):
     def get_queryset(self):
         t = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
         return super(TagView, self).get_queryset().filter(tags=t).order_by('-created_time')
-def tag(request, pk):
+'''def tag(request, pk):
     t = get_object_or_404(Tag, pk=pk)
     post_list = Post.objects.filter(tags=t).order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list':post_list})
-    #return render(request, 'blog/index.html', context={'hello':'Hello World!'})
+    #return render(request, 'blog/index.html', context={'hello':'Hello World!'})'''
 
 
